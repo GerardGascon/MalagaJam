11243f1474317614c0309fd9b112896f4de61cf4
Geri <ggasconmoline@gmail.com>
2024-01-27 12:16:27 +0100

    feature: Added message parser for better templating

--- Assets/Scripts/Messaging/Typer/Message.cs
+++ Assets/Scripts/Messaging/Typer/Message.cs
@@ -8,24 +8,34 @@ namespace Messaging {
 	public class Message : MonoBehaviour {
 		public RectTransform RectTransform { private set; get; }
 		public string Text => messageText.text;
+		public string RealText => realMessageText.text;
 
 		[SerializeField] private TMP_Text messageText;
+		[SerializeField] private TMP_Text realMessageText;
 
 		private TextTyper _textTyper;
+		private TextTyper _realTextTyper;
 		private Coroutine _typingCoroutine;
+		private Coroutine _realTypingCoroutine;
 
 		private void Awake() {
 			RectTransform = GetComponent<RectTransform>();
 			_textTyper = new TextTyper(messageText);
+			_realTextTyper = new TextTyper(realMessageText);
 		}
 
-		public void SetMessageText(string text, bool animate) {
-			this.EnsureCoroutineStopped(ref _typingCoroutine);
+		public void SetMessageText(string text, bool animate) => 
+			SetText(ref _typingCoroutine, messageText, _textTyper, text, animate);
+		public void SetMessageRealText(string text, bool animate) => 
+			SetText(ref _realTypingCoroutine, realMessageText, _realTextTyper, text, animate);
+
+		private void SetText(ref Coroutine routine, TMP_Text tmpText, TextTyper typer, string text, bool animate) {
+			this.EnsureCoroutineStopped(ref routine);
 
 			if (animate)
-				_typingCoroutine = StartCoroutine(_textTyper.AnimateTextIn(text, null));
+				routine = StartCoroutine(typer.AnimateTextIn(text, null));
 			else
-				messageText.text = text;
+				tmpText.text = text;
 		}
 	}
 }
--- Assets/Scripts/Messaging/Typer/MessageManager.cs
+++ Assets/Scripts/Messaging/Typer/MessageManager.cs
@@ -1,32 +1,61 @@
 using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Messaging {
-	public class MessageManager : MonoBehaviour{
+	public class MessageManager : MonoBehaviour {
 		[SerializeField] private MessageStructureGenerator messageStructureGenerator;
 
 		private Message[] _messages;
-		
+
+		[SerializeField] private TextAsset textAsset;
+
 		private void Awake() {
 			_messages = messageStructureGenerator.GenerateMessages();
 		}
 
+		private async void Start() {
+			string[] fLines = textAsset.text.Split(':');
+			int i = 0;
+			while (Application.isPlaying) {
+				Debug.Log(textAsset.text);
+				CreateMessage(MessageParser.SplitMessage(fLines[i % 2]).Key);
+				++i;
+				await Task.Delay(2000);
+			}
+		}
+
 		public void CreateMessage(string message) {
 			ModifyMessageText(message, _messages.Length - 1);
+		}
+		public void CreateRealMessage(string message) {
+			ModifyRealMessageText(message, _messages.Length - 1);
 		}
 
 		private void ModifyMessageText(string message, int index) {
 			ModifyPreviousMessageText(_messages[index].Text, index - 1);
 			_messages[index].SetMessageText(message, true);
 		}
+		private void ModifyRealMessageText(string message, int index) {
+			ModifyPreviousRealMessageText(_messages[index].Text, index - 1);
+			_messages[index].SetMessageRealText(message, true);
+		}
 
 		private void ModifyPreviousMessageText(string message, int index) {
-			if(index < 0) return;
-			
+			if (index < 0) return;
+
 			ModifyPreviousMessageText(_messages[index].Text, index - 1);
-			
+
 			_messages[index].SetMessageText(message, false);
+		}
+		
+		private void ModifyPreviousRealMessageText(string message, int index) {
+			if (index < 0) return;
+
+			ModifyPreviousRealMessageText(_messages[index].RealText, index - 1);
+
+			_messages[index].SetMessageRealText(message, false);
 		}
 	}
 }
